a268e4a63f3141fc1d518be45f2da33a8313781b
ShellDomenic <domenicaloor177@gmail.com>
2024-11-29 16:00:30 -0500

    hola Andrea

---
+++ provador2/src/main/java/com/mycompany/provador2/Provador2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.provador2;
+
+/**
+ *
+ * @author and_j
+ */
+public class Provador2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola andrea!");
+    }
+}
